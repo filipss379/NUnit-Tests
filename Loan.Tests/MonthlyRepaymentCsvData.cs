0563ab23abee0f9440e5d939c505edc7e0f24658
Filip Stasiński <filipss379@gmail.com>
2020-11-18 00:19:46 +0100

    NUnit Tests

---
+++ Loan.Tests/MonthlyRepaymentCsvData.cs
@@ -0,0 +1,35 @@
+﻿
+
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace Loan.Tests
+{
+    public class MonthlyRepaymentCsvData
+    {
+        public static IEnumerable GetTestCases(string csvFileName)
+        {
+            var csvLines = File.ReadAllLines(csvFileName);
+
+            var testCases = new List<TestCaseData>();
+
+            foreach(var line in csvLines)
+            {
+                string[] values = line.Replace(" ", "").Split(',');
+
+                decimal principal = decimal.Parse(values[0], CultureInfo.InvariantCulture);
+                decimal interestRate = decimal.Parse(values[1], CultureInfo.InvariantCulture);
+                int termInYears = int.Parse(values[2]);
+                decimal exptectedRepayment = decimal.Parse(values[3], CultureInfo.InvariantCulture);
+
+                testCases.Add(new TestCaseData(principal, interestRate, termInYears, exptectedRepayment));
+            }
+
+            return testCases;
+        }
+    }
+}
---
+++ Loan.Tests/ProductComparerShould.cs
@@ -0,0 +1,92 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loan.Tests
+{
+    [Category("Product Comparison")]
+    public class ProductComparerShould
+    {
+
+        private List<LoanProduct> products;
+        private ProductComparer sut;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            //uruchamiane raz, przed wykonaniem pierwszej metody testowej
+            // zakłąadamy, że dane tutaj nie moga być modyfikowane przez żaden test, może to popsuć pozostałe  testy
+
+            products = new List<LoanProduct>
+            {
+                new LoanProduct(1, "a", 1),
+                new LoanProduct(2, "b", 2),
+                new LoanProduct(3, "c", 3)
+            };
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            // podobnie jak one time setup uruchamia się raz na klasę, po zakończeniu ostatniego testu
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            // uruchamiane przed wykonaniem każdej metody testowej
+
+            sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Runs after each test executes
+            // sut.Dispose()
+        }
+
+        [Test]
+        public void ReturnCorrectNumberOfComparisons()
+        {
+            List<MonthlyRepaymentComparison> comparisons =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            Assert.That(comparisons, Has.Exactly(3).Items);
+        }
+
+        [Test]
+        public void NotReturnDuplicateComparisons()
+        {
+            List<MonthlyRepaymentComparison> comparisons =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            Assert.That(comparisons, Is.Unique);
+        }
+
+        [Test]
+        public void ReturnComparisonForFirstProduct()
+        {
+            List<MonthlyRepaymentComparison> comparisons =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            var exptectedProduct = new MonthlyRepaymentComparison("a", 1, 643.28m);
+            Assert.That(comparisons, Does.Contain(exptectedProduct));
+        }
+
+        [Test]
+        public void ReturnComparisonForFirstProductWithpartialKnownExpectedValues()
+        {
+            List<MonthlyRepaymentComparison> comparisons =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            Assert.That(comparisons, Has.Exactly(1)
+                                    .Matches<MonthlyRepaymentComparison>(
+                                    item => item.ProductName == "a" &&
+                                    item.InterestRate == 1 &&
+                                    item.MonthlyRepayment > 0));
+        }
+    }
+}
